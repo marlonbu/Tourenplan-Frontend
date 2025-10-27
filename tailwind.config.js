080d19103d506c6ac9109efa193155b548341fad
marlonbu <marlon@moebel-gehlenborg.de>
2025-10-27 13:36:08 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: "#0058A3",
-        grayText: "#666666"
+        gehlenborg: "#0058A3",
       },
-      fontFamily: {
-        sans: ["Inter", "ui-sans-serif", "system-ui"]
-      }
-    }
+    },
   },
-  plugins: []
+  plugins: [],
 };
