47b95c34389e7ad1e4567f077264f06e4349fe44
marlonbu <marlon@moebel-gehlenborg.de>
2025-10-28 13:09:17 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // <- WICHTIG für Render
+  base: "./", // 👈 sorgt dafür, dass Render relative Pfade nutzt
   build: {
     outDir: "dist",
+  },
+  server: {
+    port: 5173,
+    host: true,
   },
 });
