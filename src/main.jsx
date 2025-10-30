1f4a1d1d22c283f52abd57ab4597da3d22f4ea1b
marlonbu <marlon@moebel-gehlenborg.de>
2025-10-30 11:23:29 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+// ✅ WICHTIG: Leaflet CSS einmal global laden, sonst Mosaik!
+import "leaflet/dist/leaflet.css";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
